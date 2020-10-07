f2dfe096025af4acbc8a0ec921eb4704068288a0
A Ezequiel Ringuelet <ezequiel.ringuelet@fluxit.com.ar>
2020-10-07 13:26:09 -0300

    Updated docz files and static documentation

--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,7 @@
 export default {
     title: 'Trova Components Library',
     dest: '/docs',
+    base: '/trova-components',
     typescript: true,
     port: 3001,
     menu: ['Readme', 'Components'],
