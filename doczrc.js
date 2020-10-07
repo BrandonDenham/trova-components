847e82cc137627bf414bfe552e4cb8df17952923
A Ezequiel Ringuelet <ezequiel.ringuelet@fluxit.com.ar>
2020-10-07 14:17:27 -0300

    Updated docs to try to fix properties problem. Moved basic usage at the top of component docs

--- doczrc.js
+++ doczrc.js
@@ -5,4 +5,6 @@ export default {
     typescript: true,
     port: 3001,
     menu: ['Readme', 'Components'],
+    filterComponents: (files) =>
+        files.filter((filepath) => /[w-]*.(ts|tsx)$/.test(filepath)),
 };
