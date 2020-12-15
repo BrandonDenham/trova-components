f3ee944f17008c9e85f2ef8092cf455318efd18d
andres arista <andres.arista@fluxit.com.ar>
2020-12-15 16:48:16 -0300

    Rollup now builds correctly

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,28 @@
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import resolve from "@rollup/plugin-node-resolve";
-import typescript from "rollup-plugin-typescript2";
-import postcss from "rollup-plugin-postcss";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+import postcss from 'rollup-plugin-postcss';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-	input: ["src/index.tsx"],
-	output: [
-		{
-			dir: "build",
-			format: "esm",
-			sourcemap: true,
-			preserveModules: true,
-		},
-	],
-	plugins: [
-		peerDepsExternal(),
-		resolve(),
-		typescript({ useTsconfigDeclarationDir: true }),
-		postcss({
-			modules: true,
-		}),
-	],
+    input: ['src/index.tsx'],
+    output: [
+        {
+            dir: 'build',
+            format: 'esm',
+            sourcemap: true,
+            preserveModules: true,
+        },
+    ],
+    plugins: [
+        peerDepsExternal(),
+        resolve(),
+        typescript({ useTsconfigDeclarationDir: true }),
+        postcss({
+            modules: true,
+        }),
+        commonjs({
+            include: 'node_modules/**',
+        }),
+    ],
 };
