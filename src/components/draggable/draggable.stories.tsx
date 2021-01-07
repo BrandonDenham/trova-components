49f5c2b3a47301905b6690d26e91ee402fede2bf
Federico Rufrancos <federico.rufrancos@fluxit.com.ar>
2021-01-07 09:49:37 -0300

    changing button to be nested inside draggableButton

--- src/components/draggable/draggable.stories.tsx
+++ src/components/draggable/draggable.stories.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import Button from '../button/button';
 import DraggableCard from './draggableCard';
 import DraggableContainer from './draggableContainer';
 import DraggableButton from './draggableButton';
 import DraggableTarget from './draggableTarget';
-import { ButtonType } from '../button/buttonType';
-import Icon from '../icon/icon';
-import { IconName } from '../icon/iconName';
-import { Colors } from '../../shared/constants/colors';
 
 export default {
     title: 'Draggable',
@@ -37,12 +32,8 @@ export const DragButton = () => (
     <DraggableContainer>
         <DraggableButton  
             dragTargetConfiguration={{item:{ type: `string`}}}
-            onCollect={(item) => item}>
-                <Button 
-                    buttonType={ButtonType.Primary} 
-                    icon={<Icon name={IconName.Plus} color={Colors.White} />}>
-                    Transfer
-                </Button>
+            onCollect={(item) => item}
+            buttonText="Transfer">
             </DraggableButton>
     </DraggableContainer>
 );
--- src/components/draggable/draggableButton.tsx
+++ src/components/draggable/draggableButton.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import { useDrag, DragSourceMonitor } from 'react-dnd';
 import { draggableButton } from './draggableButton styles';
+import Button from '../button/button';
+import { ButtonType } from '../button/buttonType';
+import Icon from '../icon/icon';
+import { IconName } from '../icon/iconName';
+import { Colors } from '../../shared/constants/colors';
 
 import DraggableSourceProps from './draggableSource.types';
 
-const DraggableButton: React.FC<DraggableSourceProps> = ({ dragTargetConfiguration, onCollect, children }) => {
-	
-	const specs = {
-		...dragTargetConfiguration, 
-		collect: (monitor: DragSourceMonitor) => ({
-			isDragging: monitor.isDragging(),
-		})};
+const DraggableButton: React.FC<DraggableSourceProps> = ({
+    dragTargetConfiguration,
+    onCollect,
+    buttonText,
+}) => {
+    const specs = {
+        ...dragTargetConfiguration,
+        collect: (monitor: DragSourceMonitor) => ({
+            isDragging: monitor.isDragging(),
+        }),
+    };
 
-	const [collectedProps, drag] = useDrag(specs);
-	
-	return (
-		<div ref={drag} css={draggableButton()}>
-			{children}
-		</div>
-	);
-}
+    const [collectedProps, drag] = useDrag(specs);
+
+    return (
+        <div ref={drag} css={draggableButton()}>
+            <Button icon={<Icon name={IconName.Plus} color={Colors.White} />}>
+                {buttonText}
+            </Button>
+        </div>
+    );
+};
 
 export default DraggableButton;
