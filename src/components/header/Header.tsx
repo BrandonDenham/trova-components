9e5947025fe698a6f945d29bc11c26b0567831a7
Federico Rufrancos <federico.rufrancos@fluxit.com.ar>
2021-01-14 16:24:33 -0300

    updating styling and clean up code

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,22 +8,13 @@ import {
     header,
     headerTitle,
     headerDetails,
-    headerSubtitle,
-    headerSubtitle1,
-    headerIcon,
+    headerDates,
+    headerLocation,
 } from './Header.styles';
 import { Theme } from '../theme/theme.types';
 import Icon from '../icon/icon';
 import { Colors } from '../../shared/constants/colors';
 import { IconName } from '../icon/';
-import P from '../typography/p';
-
-/**
- * Renders a <Image /> component
- * @param  props
- * @param  props.children - The elements to include inside the card
- * @param  props.backgroundImage - The background color of the card. Defaults to White
- */
 
 const Header = React.forwardRef<HTMLInputElement, HeaderProps>(
     ({ title, dates, location, backgroundImage }) => {
@@ -32,15 +23,16 @@ const Header = React.forwardRef<HTMLInputElement, HeaderProps>(
             <div css={header(theme, backgroundImage)}>
                 <p css={headerTitle(theme)}>{title}</p>
                 <div css={headerDetails(theme)}>
-                    <span css={headerSubtitle(theme)}>{dates}</span>
-                    <span css={headerSubtitle1(theme)}>
-                        <Icon
-                            css={headerIcon(theme)}
-                            name={IconName.Location}
-                            color={Colors.Primary}
-                        />
-                        {location}
-                    </span>
+                    <span css={headerDates(theme)}>{dates}</span>
+                    {location && (
+                        <span css={headerLocation(theme)}>
+                            <Icon
+                                name={IconName.Location}
+                                color={Colors.Primary}
+                            />
+                            {location}
+                        </span>
+                    )}
                 </div>
             </div>
         );
--- src/components/header/Header.types.ts
+++ src/components/header/Header.types.ts
@@ -1,11 +1,6 @@
-import { SerializedStyles } from '@emotion/core';
-import { ReactNode } from 'react';
-import { Colors } from '../../shared/constants/colors';
-
 export default interface HeaderProps {
     title: string;
     dates: string;
-    location: string;
+    location?: string;
     backgroundImage: any;
-    children?: ReactNode;
 }
