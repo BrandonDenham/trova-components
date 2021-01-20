6266f0536179b31458403e9f987eb39fbf670127
Federico Rufrancos <federico.rufrancos@fluxit.com.ar>
2021-01-20 09:00:30 -0300

    adding missing components to be exported

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import Sidebar, { SidebarItem, SidebarMenu } from './components/sidebar';
 import MainNavigation, {
     MainNavigationItem,
 } from './components/mainNavigation';
-import Button, { ButtonType } from './components/button';
+import Button, { ButtonType, DraggableButton } from './components/button';
 import Card from './components/card';
 import Icon, { IconName, IconSize } from './components/icon';
 import Input from './components/input';
@@ -14,6 +14,11 @@ import { H1, H2, H3, H4, P, ParagraphSize } from './components/typography';
 import Dropdown, { MultipleDropdown } from './components/dropdown';
 import ThemeWrapper from './components/themeWrapper';
 import Header from './components/header/Header';
+import DraggableContainer from './components/draggable/draggableContainer';
+import DraggableCard from './components/card/draggableCard';
+import DraggableTarget from './components/draggable/draggableTarget';
+import { Colors } from './shared/constants/colors';
+import { ComponentWidth } from './components/input/componentWidth';
 
 export {
     Sidebar,
@@ -42,4 +47,10 @@ export {
     MultipleDropdown,
     ThemeWrapper,
     Header,
+    DraggableButton,
+    DraggableContainer,
+    DraggableCard,
+    DraggableTarget,
+    Colors,
+    ComponentWidth,
 };
