13ce6527ae2f5b75a0c9fa93b1bc96eaf6d05434
andres arista <andres.arista@fluxit.com.ar>
2021-02-09 10:40:11 -0300

    Header component refactor

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -11,24 +11,28 @@ import {
     headerLocation,
 } from './Header.styles';
 
-const Header = React.forwardRef<HTMLInputElement, HeaderProps>(
-    ({ title, subtitle, iconDescription, backgroundImage, icon, className }) => {
-        const theme = useTheme();
-        return (
-            <div css={header(theme, backgroundImage)} className={className}>
-                <p css={headerTitle(theme)}>{title}</p>
-                <div css={headerDetails(theme)}>
-                    <span css={headerDates(theme)}>{subtitle}</span>
-                    {iconDescription && (
-                        <span css={headerLocation(theme)}>
-                            {icon && icon}
-                            {iconDescription}
-                        </span>
-                    )}
-                </div>
+const Header: React.FC<HeaderProps> = ({
+    title,
+    subtitle,
+    iconDescription,
+    backgroundImage,
+    icon,
+    className,
+}) => {
+    const theme = useTheme();
+    return (
+        <div css={header(theme, backgroundImage)} className={className}>
+            <p css={headerTitle(theme)}>{title}</p>
+            <div css={headerDetails(theme)}>
+                <span css={headerDates(theme)}>{subtitle}</span>
+                {iconDescription && (
+                    <span css={headerLocation(theme)}>
+                        {icon && icon}
+                        {iconDescription}
+                    </span>
+                )}
             </div>
-        );
-    }
-);
-
+        </div>
+    );
+};
 export default Header;
