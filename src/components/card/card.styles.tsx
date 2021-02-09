7da39ec1ba00cdc19073b203340b9c07bdd44366
Andres Arista <30939121+andres-arista@users.noreply.github.com>
2021-02-09 10:13:51 -0300

    Changes necessary for sticky panels update (#38)
    Co-authored-by: andres arista <andres.arista@fluxit.com.ar>

--- src/components/card/card.styles.tsx
+++ src/components/card/card.styles.tsx
@@ -7,9 +7,7 @@ export const card = (theme: Theme, backgroundColor: Colors) => css`
     padding: 0.9rem 1rem 1.2rem 1.2rem;
     border-radius: 11px;
     background-color: ${backgroundColor};
-    margin: 0.25rem 0rem;
     @media (max-width: ${theme.breakpoints.sm}px) {
         padding: 0.9rem 1rem 1.3rem 1rem;
-        margin: 0.5rem 1.2rem;
     }
 `;
