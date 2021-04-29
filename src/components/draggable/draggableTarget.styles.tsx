e1afbb7b317fce7e97526e204b6febf228f473da
Augusto E Ringuelet <eze87lp@gmail.com>
2021-04-29 14:01:05 -0600

    added the isDragging and isOver events to the draggable container to show different CSS in those cases and improve the UX experience

--- src/components/draggable/draggableTarget.styles.tsx
+++ src/components/draggable/draggableTarget.styles.tsx
@@ -2,15 +2,25 @@ import { css } from '@emotion/react';
 import { Colors } from '../../shared/constants/colors';
 
 export const draggableTarget = (
-    cursor: string|undefined = undefined,
-    backgroundColor: Colors = Colors.PrimaryContentBackground
-) => css`
-    display: flex;
-    flex-direction: column;
-    border: 1px dashed ${Colors.Gray};
-    border-radius: 0.6rem;
-    cursor: ${cursor};
-    background-color: ${backgroundColor};
-    margin: 0.5rem 0rem;
-    padding: 1rem;
-`;
+    cursor: string | undefined = undefined,
+    backgroundColor: Colors = Colors.PrimaryContentBackground,
+    isOver: boolean | undefined = false,
+    isDragging: boolean | undefined = false
+) => {
+    let actualBackgroundColor = backgroundColor;
+    if (isOver) {
+        actualBackgroundColor = Colors.LightBlue;
+    } else if (isDragging) {
+        actualBackgroundColor = Colors.LightOrange;
+    }
+    return css`
+        display: flex;
+        flex-direction: column;
+        border: 1px dashed ${Colors.Gray};
+        border-radius: 0.6rem;
+        cursor: ${cursor};
+        background-color: ${actualBackgroundColor};
+        margin: 0.5rem 0rem;
+        padding: 1rem;
+    `;
+};
