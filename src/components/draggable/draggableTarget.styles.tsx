fbb6fdc097142d5da89871beb63ac7ec94e7a3e7
Julieta <julieta.gobbi@fluxit.com.ar>
2021-05-27 13:27:10 -0300

    Changes in function paremters to avoid undefined values. Update library

--- src/components/draggable/draggableTarget.styles.tsx
+++ src/components/draggable/draggableTarget.styles.tsx
@@ -2,44 +2,39 @@ import { css } from '@emotion/react';
 import { Colors } from '../../shared/constants/colors';
 
 export const draggableTarget = (
-    cursor: string | undefined = undefined,
-    color: Colors = Colors.DarkGray,
-    backgroundColor: Colors = Colors.LightSilver,
-    boxShadow: string | undefined = 'none',
     isOver: boolean | undefined = false,
     isDragging: boolean | undefined = false
 ) => {
-    let actualColor = color;
-    let actualBackgroundColor = backgroundColor;
-    let actualBorder = Colors.Gray;
-    let actualBoxShadow = boxShadow;
+    let color = Colors.DarkGray;
+    let backgroundColor = Colors.LightSilver;
+    let border = Colors.Gray;
+    let boxShadow = 'none';
 
     if (isOver) {
-        actualColor = Colors.DarkPrimary;
-        actualBackgroundColor = Colors.LightSecondary;
-        actualBorder = Colors.Primary;
-        actualBoxShadow =
+        color = Colors.DarkPrimary;
+        backgroundColor = Colors.LightSecondary;
+        border = Colors.Primary;
+        boxShadow =
             'inset 0 1px 0 #dc5f4fa3, inset 1px 0px 0 #dc5f4fa8, 0 2px 2px 0 #f65c4e40, 0 0px 3px 1px #dc5f4f57';
     } else if (isDragging) {
-        actualColor = Colors.DarkPrimary;
-        actualBackgroundColor = Colors.ExtraLightSecondary;
-        actualBorder = Colors.DarkPrimary;
-        actualBoxShadow =
+        color = Colors.DarkPrimary;
+        backgroundColor = Colors.ExtraLightSecondary;
+        border = Colors.DarkPrimary;
+        boxShadow =
             'inset 0 0px 2px 0 #f65c4e1a, inset 0 0px 3px 2px #dc5f4f21, 0 1px 0px 0 #f65c4e21, 0 0px 0px 1px #dc5f4f3b';
     }
 
     return css`
         display: flex;
         flex-direction: column;
-        border: 1px dashed ${actualBorder};
-        box-shadow: ${actualBoxShadow};
+        border: 1px dashed ${border};
+        box-shadow: ${boxShadow};
         border-radius: 0.6rem;
-        cursor: ${cursor};
-        background-color: ${actualBackgroundColor};
+        background-color: ${backgroundColor};
         margin: 0.5rem 0rem;
         padding: 1rem;
         p {
-            color: ${actualColor};
+            color: ${color};
         }
     `;
 };
