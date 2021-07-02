b25c4af728a8b171b1f8eafcb7304f9c9dfd3be9
julieta <67168347+juligobbi-fluxit@users.noreply.github.com>
2021-07-02 10:57:02 -0300

    fix the width of the mainContent when is empty (#110)

--- src/components/mainContent/mainContent.styles.tsx
+++ src/components/mainContent/mainContent.styles.tsx
@@ -6,6 +6,7 @@ export const mainContent = (theme: Theme) => css`
     border-radius: 35px 0 0 0;
     background-color: ${Colors.PrimaryContentBackground};
     font-family: ${theme.fonts.robotoBold};
+    width: 100%;
     @media (max-width: ${theme.breakpoints.md}px) {
         border-radius: 23px 23px 0 0;
         padding: 6rem 1rem 2rem 1rem;
