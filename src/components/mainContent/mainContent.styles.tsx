1f2117d141d179bb63df9433c954c89300f61202
julieta <67168347+juligobbi-fluxit@users.noreply.github.com>
2021-07-12 10:15:47 -0300

    Restyle header photo to have a full cover on mobile (#112)

--- src/components/mainContent/mainContent.styles.tsx
+++ src/components/mainContent/mainContent.styles.tsx
@@ -8,8 +8,8 @@ export const mainContent = (theme: Theme) => css`
     font-family: ${theme.fonts.robotoBold};
     width: 100%;
     @media (max-width: ${theme.breakpoints.md}px) {
-        border-radius: 23px 23px 0 0;
-        padding: 6rem 1rem 2rem 1rem;
+        border-radius: 0;
+        padding: 5rem 1rem 2rem 1rem;
     }
     @media (max-width: ${theme.breakpoints.sm}px) {
         padding: 2rem 1rem 5rem 1.5rem;
